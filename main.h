95b50b46cdc421d097d84b3f9611b00babb4eb68
Dhoryaay <40821880+Dhoryaay@users.noreply.github.com>
2022-08-09 11:41:16 +0100

    Update main.h

--- main.h
+++ main.h
@@ -19,24 +19,24 @@
 #define S_SHORT 1
 
 /**
- *  * struct fmt - Struct op
- *   *
- *    * @fmt: The format.
- *     * @fn: The function associated.
- *      */
+ *  struct fmt - Struct op
+ * 
+ *  @fmt: The format.
+ *  @fn: The function associated.
+ */
 struct fmt
 {
-		char fmt;
-			int (*fn)(va_list, char[], int, int, int, int);
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
 };
 
 
 /**
- *  * typedef struct fmt fmt_t - Struct op
- *   *
- *    * @fmt: The format.
- *     * @fm_t: The function associated.
- *      */
+   * typedef struct fmt fmt_t - Struct op
+    *
+     * @fmt: The format.
+      * @fm_t: The function associated.
+       */
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
